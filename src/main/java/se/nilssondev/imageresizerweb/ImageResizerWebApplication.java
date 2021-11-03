f4dc5f377f4e835e994af1add92bd8c1f23b7084
Adde86 <57154481+Adde86@users.noreply.github.com>
2021-11-03 09:15:57 +0100

    added dummy code

---
+++ src/main/java/se/nilssondev/imageresizerweb/ImageResizerWebApplication.java
@@ -0,0 +1,13 @@
+package se.nilssondev.imageresizerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageResizerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageResizerWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/nilssondev/imageresizerweb/ImageResizerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nilssondev.imageresizerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageResizerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
