c3afcd831246dcee6cb8078a90b2b4c7e909d380
Adde86 <57154481+Adde86@users.noreply.github.com>
2021-11-05 15:14:28 +0100

    added dummy code

--- src/main/java/se/nilssondev/imageresizerweb/controllers/Controller.java
+++ src/main/java/se/nilssondev/imageresizerweb/controllers/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
 
     @GetMapping("/")
     public String index(){
-        return "Hello aws how you doing today?";
+        return "Index has been changed";
     }
 
     @GetMapping("/test")
