bb8696d2708bde65f581242c50c656937390ce5d
Adde86 <57154481+Adde86@users.noreply.github.com>
2021-11-11 14:32:53 +0100

    reworked some tests

--- src/main/java/se/nilssondev/imageresizerweb/ImageResizerWebApplication.java
+++ src/main/java/se/nilssondev/imageresizerweb/ImageResizerWebApplication.java
@@ -2,16 +2,12 @@ package se.nilssondev.imageresizerweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ImageResizerWebApplication {
 
     public static void main(String[] args) {
-        SpringApplicationBuilder app = new SpringApplicationBuilder(ImageResizerWebApplication.class);
-        app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
-        app.run();
+        SpringApplication.run(ImageResizerWebApplication.class, args);
     }
 
 }
